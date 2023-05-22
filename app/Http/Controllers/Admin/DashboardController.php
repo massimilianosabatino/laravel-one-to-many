4bf0e259e85bd80ee4011595a001a6ffbd4828f9
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-22 21:16:32 +0200

    fix: dashboard count

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,9 +14,10 @@ class DashboardController extends Controller
         $countProjectTotal = DB::table('projects')->count('id');
 
         $countMaxOnCategory = DB::table('projects')->select(DB::raw('count(id) as number, type_id'))->groupBy('type_id')->get();
-        $getMaxCategory = max($countMaxOnCategory->all());        
+        $getMaxCategory = max($countMaxOnCategory->all());
+        $getMaxCategoryName = DB::table('types')->find($getMaxCategory->type_id);
 
 
-        return view('admin.dashboard', compact('countProjectTotal', 'getMaxCategory'));
+        return view('admin.dashboard', compact('countProjectTotal', 'getMaxCategory', 'getMaxCategoryName'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -28,7 +28,8 @@
                         Totale progetti: {{ $countProjectTotal }}
                     </div>
                     <div class="col-6">
-                        Categoria con più progetti: {{Str::ucfirst($getMaxCategory->type_id) }} -> {{ $getMaxCategory->number }} progetti
+                        
+                        Categoria con più progetti: {{Str::ucfirst($getMaxCategoryName->category) }} -> {{ $getMaxCategory->number }} progetti
                     </div>
                     <div class="mt-2">
                         <a href="{{ route('admin.projects.index') }}" class="btn btn-light">Gestione progetti</a>
