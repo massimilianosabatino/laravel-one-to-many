2c9f6bef8f1af0593c0e566de53bffb8dcf00142
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-23 01:02:15 +0200

    add: show category

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -54,7 +54,7 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
-        //
+        return view('admin.type.show', compact('type'));
     }
 
     /**
--- resources/views/admin/type/show.blade.php
+++ resources/views/admin/type/show.blade.php
@@ -5,13 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
 
-            <img src="@if (Str::startsWith($project->cover, 'http')) {{ $project->cover }} @else {{ asset('storage/' . $project->cover) }} @endif" alt="" class="img-fluid">
-            <h1>{{ $project->title }}</h1>
-            <p>{{ $project->description }}</p>
-            <div>
-                Category: {{ Str::ucfirst($project->type?->category) ?: 'Nessuna categoria' }}
-            </div>
-                <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Back</a>
+            
+            <h1>{{ $type->category }}</h1>
+            <p>ID categorya: {{ $type->id }}</p>
+                <a href="{{ route('admin.types.index') }}" class="btn btn-secondary">Back</a>
         </div>
     </div>
 </div>
